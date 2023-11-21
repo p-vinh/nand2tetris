ea56fad80e473999eabf4b7bf765b510dc276e76
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-11-20 23:00:02 -0800

    Initial Commit/Setup

--- projects/10/SyntaxAnalyzer/JackAnalyzer.java
+++ projects/10/SyntaxAnalyzer/JackAnalyzer.java
@@ -1,36 +1,62 @@
 package SyntaxAnalyzer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JackAnalyzer {
-    
+    private static String fileName;
+    private static JackTokenizer tokenizer;
+    private static PrintWriter writer;
+    public static void translateFile() {
+        try {
+            writer = new PrintWriter(new File(fileName + "T.xml"));
+
+            while (tokenizer.hasMoreTokens()) {
+                tokenizer.advance();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            writer.close();
+        }
+    }
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: java JackAnalyzer <file.jack|dir>");
-            System.exit(1);
-        }
+        // if (args.length != 1) {
+        //     System.out.println("Usage: java JackAnalyzer <file.jack|dir>");
+        //     System.exit(1);
+        // }
 
-        File file = new File(args[0]);
+        // File file = new File(args[0]);
 
-        if (file.isDirectory())
-            for (File f : file.listFiles()) {
-                if (f.getName().endsWith(".jack")) {
-                    try {
-                        // Run tokenizer
-                        JackTokenizer tokenizer = new JackTokenizer(f);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        else {
-            try {
-                JackTokenizer tokenizer = new JackTokenizer(file);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        // if (file.isDirectory())
+        //     for (File f : file.listFiles()) {
+        //         if (f.getName().endsWith(".jack")) {
+        //             try {
+        //                 // Run tokenizer
+        //                 JackTokenizer tokenizer = new JackTokenizer(f);
+        //             } catch (IOException e) {
+        //                 e.printStackTrace();
+        //             }
+        //         }
+        //     }
+        // else {
+        //     try {
+        //         JackTokenizer tokenizer = new JackTokenizer(file);
+
+        //     } catch (IOException e) {
+        //         e.printStackTrace();
+        //     }
+        // }
+
+        try {
+            tokenizer = new JackTokenizer(new File("projects\\10\\ArrayTest\\Main.jack"));
+            translateFile();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     
--- projects/10/SyntaxAnalyzer/JackTokenizer.java
+++ projects/10/SyntaxAnalyzer/JackTokenizer.java
@@ -2,10 +2,145 @@ package SyntaxAnalyzer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class JackTokenizer {
-    private File file;
+    private String fileName;
+    private String currentToken; // Current token
+    private String tokenType; // Type of current token
+    private Map<String, String> keywords;
+    private Scanner scanner;
+
     public JackTokenizer(File file) throws IOException {
-        this.file = file;
+        this.fileName = file.getName();
+        this.currentToken = "";
+        initKeywords();
+
+        this.scanner = new Scanner(file);
     }
+
+    private void initKeywords() {
+        keywords = new HashMap<>();
+        keywords.put("class", "CLASS");
+        keywords.put("constructor", "CONSTRUCTOR");
+        keywords.put("function", "FUNCTION");
+        keywords.put("method", "METHOD");
+        keywords.put("field", "FIELD");
+        keywords.put("static", "STATIC");
+        keywords.put("var", "VAR");
+        keywords.put("int", "INT");
+        keywords.put("char", "CHAR");
+        keywords.put("boolean", "BOOLEAN");
+        keywords.put("void", "VOID");
+        keywords.put("true", "TRUE");
+        keywords.put("false", "FALSE");
+        keywords.put("null", "NULL");
+        keywords.put("this", "THIS");
+        keywords.put("let", "LET");
+        keywords.put("do", "DO");
+        keywords.put("if", "IF");
+        keywords.put("else", "ELSE");
+        keywords.put("while", "WHILE");
+        keywords.put("return", "RETURN");
+    }
+
+
+
+    /*
+     * Gets the next token from the input and makes it the current token. This
+     * method should only be called if hasMoreTokens() is true. Initially there is
+     * no current token.
+     */
+    public void advance() {
+        scanner.useDelimiter("\\s+|\\n|\\r|\\t|\\{|\\}|\\(|\\)|\\[|\\]|\\.|,|;|\\+|-|\\*|\\/|\\||=|~");
+        
+        if (hasMoreTokens()) {
+            currentToken = scanner.next();
+
+            if (keywords.containsKey(currentToken)) {
+                currentToken = keywords.get(currentToken);
+
+            }
+            else if (currentToken.matches("\\d+"))
+                currentToken = "INT_CONST";
+            else if (currentToken.matches("\".*\""))
+                currentToken = "STRING_CONST";
+            else if (currentToken.matches("\\w+"))
+                currentToken = "IDENTIFIER";
+            else
+                currentToken = "SYMBOL";
+        }
+    }
+
+    /*
+     * Returns true if there are more tokens in the input.
+     */
+    public boolean hasMoreTokens() {
+        return scanner.hasNext();
+    }
+
+    /*
+     * Returns the type of the current token, as a constant.
+     */
+    public String tokenType() {
+        return currentToken;
+    }
+
+    /*
+     * Returns the keyword which is the current token as a constant. Should be called only when
+     * tokenType() is KEYWORD.
+     */
+    public String keyWord() {
+        return "";
+    }
+
+    /*
+     * Returns the character which is the current token. Should be called only when
+     * tokenType() is SYMBOL.
+     */
+    public char symbol() {
+        if (tokenType().equals("SYMBOL"))
+            throw new IllegalStateException("Current token is not a symbol");
+        return currentToken.charAt(0);
+    }
+
+    /*
+     * Returns the identifier or integer value of the current token. Should be
+     * called only when tokenType() is IDENTIFIER or INT_CONST.
+     */
+    public String identifier() {
+        if (tokenType().equals("IDENTIFIER"))
+            return currentToken;
+        else if (tokenType().equals("INT_CONST"))
+            return currentToken;
+        else
+            throw new IllegalStateException("Current token is not an identifier or integer");
+    }
+
+    /*
+     * Returns the integer value of the current token. Should be called only when
+     * tokenType() is INT_CONST.
+     */
+    public int intVal() {
+        if (tokenType().equals("INT_CONST"))
+            return Integer.parseInt(currentToken);
+        else
+            throw new IllegalStateException("Current token is not an integer");
+    }
+
+    /*
+     * Returns the string value of the current token, without the double quotes.
+     * Should be called only when tokenType() is STRING_CONST.
+     */
+    public String stringVal() {
+        if (tokenType().equals("STRING_CONST"))
+            return currentToken.substring(1, currentToken.length() - 1);
+        else
+            throw new IllegalStateException("Current token is not a string");
+    }
+
+
 }
